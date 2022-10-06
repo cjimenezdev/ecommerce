47502b970c6a75b151bbdb7d6caa6358731f25a5
Carlos Jiménez <carlosjimenez32077@gmail.com>
2022-10-06 16:02:47 -0500

    Update web-index.php

--- view/page/web-index.php
+++ view/page/web-index.php
@@ -451,5 +451,7 @@ echo '<script>window.location.href="./home/"</script>;';
 </main>
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
         class="bi bi-arrow-up-short"></i></a>
+/*---------- Scripts ----------*/
+<?php  include "./view/inc/scripts.php"; ?>
 
 <?php } ?>
