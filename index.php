91724cfc37b6ae99349619e9d417a978261c6161
Carlos Jiménez <carlosjimenez32077@gmail.com>
2022-10-06 15:45:31 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php header('Location: ../'); ?>
+<?php
+	require_once "./config/app.php";
+	require_once "./controller/config/viewsController.php";
+
+	$plantilla = new vistasControlador();
+	$plantilla->obtener_plantilla_controlador();
